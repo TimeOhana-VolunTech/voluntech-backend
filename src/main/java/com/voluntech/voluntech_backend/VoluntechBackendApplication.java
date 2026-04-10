24afc3288203b77e6ee0de74bddc6594e25b2862
Marcos <maik.cok14@gmail.com>
2026-04-10 00:57:40 -0300

    chore: setup inicial spring boot com postgres

---
+++ src/main/java/com/voluntech/voluntech_backend/VoluntechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.voluntech.voluntech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voluntech/voluntech_backend/VoluntechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voluntech.voluntech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
